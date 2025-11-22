541545406826e34d56c71f115cd40e8b39226f33
mandeeep03 <mandeepbhatt52@gmail.com>
2025-11-22 23:50:18 +0530

    Changed border

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
     <footer
         className="w-full  py-4 px-6 md:px-12
-        bg-white/10 backdrop-blur-xl border border-white/20
+        bg-white/10 backdrop-blur-xl
         shadow-[0_8px_30px_rgba(0,0,0,0.12)] fixed bottom-0 left-0"
     >
         <div className="flex justify-around my-8 sm:my-16">
