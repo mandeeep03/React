332dce5c28e7a3bb3054da4163f2f2ef89f5fc54
mandeeep03 <mandeepbhatt52@gmail.com>
2025-11-22 20:20:56 +0530

    Add serve for production deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
-    allowedHosts:true,
-  },
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  }
 })
