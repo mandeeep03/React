085e5f7b3722cc9fcabbc4b07f018ada260df883
mandeeep03 <mandeepbhatt52@gmail.com>
2025-11-23 18:20:29 +0530

    removed redundency

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,15 +1,15 @@
 function Container(props) {
-    let name;
+    let name =
+      "w-full border-8 border-black/20 rounded-lg";
     switch (props.index) {
       case "1":
-        name = "w-full md:w-[90%]  h-[30rem] border-8 border-black/20 rounded-lg";
+        name += " md:w-[90%]  h-[30rem] ";
         break;
       case "2":
-        name = "w-full md:w-[45%]  h-[30rem] border-8 border-black/20 rounded-lg";
+        name += " md:w-[45%]  h-[30rem] ";
         break;
       case "3":
-        name = "w-full md:w-[30%]  h-[20rem] border-8 border-black/20 rounded-lg";
-        break;
+        name += " md:w-[30%]  h-[20rem] ";
     }
   return <>
     <div className={name}></div>
